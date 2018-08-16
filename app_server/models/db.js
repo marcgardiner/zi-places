069e509fd469d40c3c0aeccf658b4ed87dcec2c5
marcgardiner <marcgardinerdriven@gmail.com>
2018-08-16 13:59:52 +1000

    add event handler for mongoose and node process

---
+++ app_server/models/db.js
@@ -0,0 +1,43 @@
+var mongoose = require('mongoose');
+var gracefulShutdown;
+var dbURI = 'mongodb://localhost/ziplaces';
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', function() {
+    console.log('Mongoose connected to ' + dbURI);
+});
+
+mongoose.connection.on('error', function(err) {
+    console.log('Mongoose connection error: ' + err);
+});
+
+mongoose.connection.on('disconnected', function() {
+    console.log('Mongoose disconnected');
+});
+
+gracefulShutdown = function(msg, callback) {
+    mongoose.connection.close(function() {
+        console.log('Mongoose disconnected through ' + msg);
+        callback();
+    });
+};
+
+// nodemon restart handler
+process.once('SIGUSR2', function() {
+    gracefulShutdown('nodemon restart', function() {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+
+// app terminiated handler
+process.once('SIGINT', function() {
+    gracefulShutdown('app terminated', function() {
+        process.exit(0);
+    });
+});
+
+process.on('SIGTERM', function() {
+    gracefulShutdown('Heroku app showdown', function() {
+        process.exit(0);
+    });
+});
