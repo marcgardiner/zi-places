28a5da37f461276aa22e0516a98565062e2c03f5
marcgardiner <marcgardinerdriven@gmail.com>
2018-08-20 15:14:52 +1000

    add location integration with api

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,5 +1,42 @@
+var request = require('request');
+var apiOptions = {
+    server: "http://localhost:3000"
+};
+
+if (process.env.NODE_ENV === 'production') {
+    apiOptions.server = "http://heroku.com";
+}
+
+var renderHomepage = function(req, res, responseBody) {
+    res.render('locations-list', {
+        title: "find a place to work with wifi",
+        pageHeader: {
+            title: 'zi-places',
+            strapline: 'Find places to work with wifi near you'
+        },
+        sidebar: "Looking for wifi and a seat?",
+        locations: responseBody
+    });
+};
+
 module.exports.homeList = function(req, res) {
-    res.render('locations-list', {title: 'Home'});
+    var requestOptions, path;
+    path = '/api/locations';
+    requestOptions = {
+        url: apiOptions.server + path,
+        method: "GET",
+        json: {},
+        qs: {
+            lng: -0.792,
+            lat: 51.37,
+            maxDistance: 20
+        }
+    };
+    request(requestOptions,
+        function(err, response, body) {
+            renderHomepage(req, res, body);
+        }
+    );
 };
 
 module.exports.locationInfo = function(req, res) {
